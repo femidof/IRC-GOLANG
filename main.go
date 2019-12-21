0e0bed76478e730a69c1c6d069b3f27ce3af80b1
Olorunfemi Durosinmi <oldurosi@e1z3r7p21.42.us.org>
2019-12-21 04:40:56 -0800

    Beginning Stage

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+type User struct {
+	username string
+	nickname string
+	password string
+	channel  ChatChannel
+}
+
+type ChatServer struct {
+}
+
+type ChatChannel struct {
+	Name  string
+	Users map[string]User
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handleconn(conn)
+	}
+}
+
+func handleconn(conn net.Conn) {
+	// err := conn.SetDeadline(time.Now().Add(20 * time.Second)) //Set up a timeout
+	// if err != nil {
+	// 	log.Println("CONNECTION TIMEOUT")
+	// }
+	scanner := bufio.NewScanner(conn) //reads user input
+	for scanner.Scan() {              //loops through as long as it takes
+		ln := scanner.Text()                           //parses the input to ln
+		fmt.Println(ln)                                //displays on the server
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln) //displays on the conn client
+	}
+	defer conn.Close()
+
+	fmt.Println("Code Got To The Termination")
+}
